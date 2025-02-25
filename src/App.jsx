9c75e21e63ff1e0ccd10b877a185844fb23fb1c3
Athena Nefos <athena.nefos@gmail.com>
2025-02-25 12:12:32 -0500

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import Home from './pages/Home.jsx';
-import Search from './pages/Search.jsx';
-import MovieDetails from './pages/MovieDetails.jsx';
-import Collection from './pages/Collection.jsx';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import MovieDetails from './pages/MovieDetails';
+import Collection from './pages/Collection';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PrivateRoute from './components/PrivateRoute';
+import { AuthProvider } from './context/AuthContext';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/movie/:id" element={<MovieDetails />} />
-            <Route path="/collection" element={<Collection />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="app-container">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              
+              {/* Protected Routes */}
+              <Route element={<PrivateRoute />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/movie/:id" element={<MovieDetails />} />
+                <Route path="/collection" element={<Collection />} />
+              </Route>
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
